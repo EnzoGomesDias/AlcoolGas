1c146a1ce534cdcbbacf9968444f46b521cb361a
EnzoGomesDias <48141468+EnzoGomesDias@users.noreply.github.com>
2019-03-01 21:52:55 -0300

    Add files via upload

---
+++ AlcoolGas/src/AlcoolGas.java
@@ -0,0 +1,12 @@
+
+public class AlcoolGas {
+public static void main(String[] args){
+	double alcool = 2.39;
+	double gas = 3.19;
+	
+	if(gas*0.7>alcool) {System.out.println("Coloque Alcool");}
+	if(gas*0.7<alcool) {System.out.println("Coloque Gasolina");}
+	
+	}
+}
+
